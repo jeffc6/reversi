fd27157c8fe83612ce624ed328e6be07580199bc
Jeff Chen <>
2018-06-03 12:51:37 +0900

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -301,6 +301,9 @@ $(function(){
 	
 	console.log('*** Client Log Message: \'join_room\ payload: ' + JSON.stringify(payload));
 	socket.emit('join_room', payload);
+
+	$('#quit').append('<a href="lobby.html?username='+username+'" class="btn btn-danger btn-default active" role="button" aria-pressed="true">Quit</a>');
+
 });
 
 var old_board = [
@@ -314,7 +317,7 @@ var old_board = [
 					['?','?','?','?','?','?','?','?',]
 ];
 
-var my_colo = ' ';
+var my_color = ' ';
 
 
 socket.on('game_update',function(payload){
@@ -353,10 +356,17 @@ socket.on('game_update',function(payload){
 
 
 	/* Animate changes to the board*/
-
+	var blacksum = 0;
+	var whitesum = 0;
 	var row,column;
-	for(row = 0; row < 8; row++){
-		for(column = 0; column <8; column++){
+	for(row=0; row<8; row++){
+		for (column=0; column<8; column++){
+			if (board[row][column] === 'b'){
+				blacksum++;
+			}
+			if (board[row][column] === 'w'){
+				whitesum++;
+			}
 			/* If a board space has changed*/
 			if(old_board[row][column] != board [row][column]){
 				if(old_board[row][column] == '?' && board[row][column] == ' '){
@@ -412,8 +422,9 @@ socket.on('game_update',function(payload){
 			}	
 		}
 	}
-
-		old_board = board;
+	$('#blacksum').html(blacksum);
+	$('#whitesum').html(whitesum);
+	old_board = board;
 });
 
 socket.on('play_token_response',function(payload){
@@ -426,7 +437,19 @@ socket.on('play_token_response',function(payload){
 	}
 });
 
-
+socket.on('game_over', function(payload){
+	console.log('*** Client Log Message: \'game_over\'\n\tpayload: '+JSON.stringify(payload));
+	/** Check for a good play_token_response **/
+	if (payload.result === 'fail') {
+		console.log(payload.message);
+		alert(payload.message);
+		return;
+	}
+	
+	/* Jump to a new page */
+	$('#game_over').html('<h1>Game Over</h1><h2>'+payload.who_won+' won!</h2>');
+	$('#game_over').append('<br><a href="lobby.html?username='+username+'" class="btn btn-success btn-lg active" role="button" aria-pressed="true">Return to the lobby</a>');
+});
 
 
 
